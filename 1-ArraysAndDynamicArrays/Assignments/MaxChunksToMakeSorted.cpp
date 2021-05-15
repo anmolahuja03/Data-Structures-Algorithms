60c2e1df4f9a1a2ec25a9eb3169a4e92e672f5d7
Anmol Ahuja <ahuja.anmol03@gmail.com>
2021-05-16 03:10:19 +0530

    added few solutions

---
+++ 1-ArraysAndDynamicArrays/Assignments/MaxChunksToMakeSorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+       // Brute Force Worst Case T.C would take O(n^2)
+
+       // Effecient Approach - O(n)
+        int currmax = INT_MIN;
+        int ans=0;
+        for(int i=0;i<arr.size();i++){
+            currmax=max(currmax,arr[i]);
+            if(currmax==i)
+                ans++;   //break a chunk at this point
+        }
+        return ans;
+    }
+};
---
+++ 1-ArraysAndDynamicArrays/Assignments/RotateArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+       /*
+        //TC= O(n^2)        
+        int i,j,temp;
+        int n=nums.size();
+        for(j=0;j<k%n;j++){
+            temp=nums[n-1];
+            for(i=n-2;i>=0;i--){
+                nums[i+1]=nums[i];
+            }
+            nums[0]=temp;
+        }
+        cout << nums[j];
+      */
+        
+        //Effecient Approach
+        int i, j;
+        int n=nums.size();
+        int temp[n];
+        for(i=0;i<n;i++){
+            temp[(i+k)%n]=nums[i];
+        }
+        for(i=0;i<n;i++){
+            nums[i]=temp[i];
+        }
+     }
+};
